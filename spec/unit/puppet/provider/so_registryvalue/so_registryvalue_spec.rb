113c8853ab0bb5d0e299837f5bfdc54a1ac8cc96
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-10-04 10:56:18 +0200

    ♻ cleanup and refactor

--- spec/unit/puppet/provider/so_registryvalue/so_registryvalue_spec.rb
+++ spec/unit/puppet/provider/so_registryvalue/so_registryvalue_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 require 'stringio'
 
 describe Puppet::Type.type(:so_registryvalue).provider(:so_registryvalue) do
-   #     {"DisplayName"=>"Audit: Audit the use of Backup and Restore privilege",
-   #      "name"=>"MACHINE\\System\\CurrentControlSet\\Control\\Lsa\\FullPrivilegeAuditing",
-   #      "reg_type"=>"3"
-   #     },
 
     let(:params) do
         {
@@ -19,15 +15,12 @@ describe Puppet::Type.type(:so_registryvalue).provider(:so_registryvalue) do
     let(:resource) do
         Puppet::Type.type(:so_registryvalue).new(params)
     end
+
     let (:hashresource) do
-      [params].each_with_object({}) do |params1, h| 
-          h[params1[:title]] = Puppet::Type.type(:so_registryvalue).new(params1) 
-      end 
-
+        {
+            params[:title] => resource,
+        }
     end
-    #@provider = provider.new(resource)
-    #resource.provider = @provider
-
 
     let(:provider) do
         resource.provider
@@ -43,11 +36,7 @@ describe Puppet::Type.type(:so_registryvalue).provider(:so_registryvalue) do
 
     def stub_secedit_export
         ini_stub = Puppet::Util::IniFile.new(File.join(
-        File.dirname(__FILE__), "../../../../fixtures/unit/puppet/provider/so_registryvalue/so_registryvalue/rvsecurityoptionsoutput.txt"), '=')
-        myfile=File.join(
-        File.dirname(__FILE__), "../../../../fixtures/unit/puppet/provider/so_registryvalue/so_registryvalue/rvsecurityoptionsoutput.txt")
-        #$stderr.puts ini_stub.get_settings('Registry Values')
-        $stderr.puts myfile 
+            File.dirname(__FILE__), "../../../../fixtures/unit/puppet/provider/so_registryvalue/so_registryvalue/rvsecurityoptionsoutput.txt"), '=')
         expect(Puppet).to receive(:[]).at_least(:once).with(:cachedir).and_return('C:\ProgramData\PuppetLabs\Puppet\cache')
         expect(File).to receive(:open).at_least(:once).with(out_file, 'w')
         expect(provider.class).to receive(:secedit).at_least(:once).with('/export', '/cfg', out_file, '/areas', 'securitypolicy')
@@ -86,61 +75,75 @@ describe Puppet::Type.type(:so_registryvalue).provider(:so_registryvalue) do
             }
           end
         end
-        it 'doing example1' do
-          example1=@instances.find { |p| p[:name].downcase == "Interactive logon: Smart card removal behavior".downcase}
+
+        it 'should list all instances' do
+          expect(@instances.size).to eq(72)
+        end
+
+        it 'should find Interactive logon with a quoted string (type 1)' do
+          example1 = @instances.find { |p|
+              p[:name].downcase == "Interactive logon: Smart card removal behavior".downcase
+          }
           expect(example1).to eq({
             :name   => 'Interactive logon: Smart card removal behavior',
             :ensure => :present,
             :regvalue => "\"1\"",
           })
         end
-        it 'doing example3' do
-          example3=@instances.find { |p| p[:name].downcase == "Audit: Audit the use of Backup and Restore privilege".downcase}
+
+        it 'should find Audit with an integer (type 3)' do
+          example3 = @instances.find { |p|
+              p[:name].downcase == "Audit: Audit the use of Backup and Restore privilege".downcase
+          }
           expect(example3).to eq({
             :name   => 'Audit: Audit the use of Backup and Restore privilege',
             :ensure => :present,
-            :regvalue => 0,
+            :regvalue => 42,
           })
 
         end
-        it 'doing sampel4' do
-          example4=@instances.find { |p| p[:name].downcase == "Recovery console: Allow automatic administrative logon".downcase}
+
+        it 'should find Recovery console with an integer (type 4)' do
+          example4 = @instances.find { |p|
+              p[:name].downcase == "Recovery console: Allow automatic administrative logon".downcase
+          }
           expect(example4).to eq({
             :name   => 'Recovery console: Allow automatic administrative logon',
             :ensure => :present,
             :regvalue => 0,
           })
         end
-        it 'doing sample 7a' do
-          example7a=@instances.find { |p| p[:name].downcase == "System settings: Optional subsystems".downcase}
+
+        it 'should find System settings with an empty array (type 7)' do
+          example7a = @instances.find { |p| p[:name].downcase == "System settings: Optional subsystems".downcase}
           expect(example7a).to eq({
             :name   => 'System settings: Optional subsystems',
             :ensure => :present,
             :regvalue => [],
           })
         end
-        it 'doing example 7b' do
-          example7b=@instances.find { |p| p[:name].downcase == "Interactive logon: Message text for users attempting to log on".downcase}
+
+        it 'should find Interactive logon with an array (type 7)' do
+          example7b = @instances.find { |p|
+              p[:name].downcase == "Interactive logon: Message text for users attempting to log on".downcase
+          }
           expect(example7b).to eq({
             :name   => 'Interactive logon: Message text for users attempting to log on',
             :ensure => :present,
             :regvalue => ["By logging in I understand that the information on this computer and network system is Company property protected by law and that it may be accessed and used only by authorized personel and that my use may be monitored."],
           })
         end
-        it 'doing sample 7c' do
-          example7c=@instances.find { |p| p[:name].downcase == "Network access: Remotely accessible registry paths and sub-paths".downcase}
+
+        it 'should find Network access with an array (type 7)' do
+          example7c = @instances.find { |p|
+              p[:name].downcase == "Network access: Remotely accessible registry paths and sub-paths".downcase
+          }
           expect(example7c).to eq({
             :name   => 'Network access: Remotely accessible registry paths and sub-paths',
             :ensure => :present,
             :regvalue => ["System\\CurrentControlSet\\Control\\Print\\Printers", "System\\CurrentControlSet\\Services\\Eventlog", "Software\\Microsoft\\OLAP Server", "Software\\Microsoft\\Windows NT\\CurrentVersion\\Print", "Software\\Microsoft\\Windows NT\\CurrentVersion\\Windows", "System\\CurrentControlSet\\Control\\ContentIndex", "System\\CurrentControlSet\\Control\\Terminal Server", "System\\CurrentControlSet\\Control\\Terminal Server\\UserConfig", "System\\CurrentControlSet\\Control\\Terminal Server\\DefaultUserConfiguration", "Software\\Microsoft\\Windows NT\\CurrentVersion\\Perflib", "System\\CurrentControlSet\\Services\\SysmonLog"],
           })
 
-        end
-        it 'should list all instances' do
-
-      #it 'should find all instances' do
-          expect(@instances.size).to eq(72)
-      #it 'should populate the instances correctly' do
         end
     end
 end
